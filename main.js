5f005f5bb245e1057151ba5f06e728ed564c6fae
Whitney Murray <wnksmith92@yahoo.com>
2015-11-30 21:26:26 -0600

    fixed issues

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 
 var bill = prompt('What is your bill');
 var state = prompt('What state is this all going down in');
+var sub = Math.round(bill * 100)/100;
+console.log(sub);
 var tax =  '\.' + 55;
-var total ='\$' + bill + tax;
+var stateTax= Math.round(sub * tax *100)/100;
+var wiTotal = Math.round(sub * 1.055 * 100)/100;
+
+
+
 
 
 if(state === 'wi'){
-	console.log(total);
-}
-else{
-	console.log(bill);
+	console.log(sub);
+	console.log(tax);
+	console.log(wiTotal);
 }
